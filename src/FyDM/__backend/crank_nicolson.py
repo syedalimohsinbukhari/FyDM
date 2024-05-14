18a8bef989d3c6772217400bbc89326613d9586f
Syed Ali Mohsin Bukhari <syedali.b@outlook.com>
2024-05-15 04:12:43 +0500

    [oneD-MINOR-10]
    1. `crank_nicolson.py` is working for IC = x - x**2

--- src/FyDM/__backend/crank_nicolson.py
+++ src/FyDM/__backend/crank_nicolson.py
@@ -1,5 +1,7 @@
 """Created on May 12 06:18:20 2024"""
+
 import numpy as np
+from matplotlib import pyplot as plt
 
 from src.FyDM import TOLERANCE
 from src.FyDM.__backend.fdm_ import identity_matrix, initial_condition_matrix, OneDimensionalFDM, \
@@ -20,7 +22,6 @@ def one_d_crank_nicolson_solver(x_range, delta_x, delta_t, time_steps, initial_c
 
     ic_values = np.linspace(*fdm_.pde_properties[0], int(fdm_.n_steps))
     ic = initial_condition_matrix(fdm_.n_steps, initial_condition, ic_values)
-    # print(ic_values, ic)
     h_matrix = identity_matrix(fdm_.n_steps) + (k**2 * fdm_.d2_cn())
 
     pde_ = OneDimensionalPDESolver(fdm_.pde_properties,
@@ -33,44 +34,33 @@ def one_d_crank_nicolson_solver(x_range, delta_x, delta_t, time_steps, initial_c
 
 
 def exact_solution(n, x_, k, L, i_dt):
-    f1 = -2 * (-1)**n * (n * np.pi)**-1
+    # f1 = -2 * (-1)**n * (n * np.pi)**-1
+    f1 = 4 * (1 - (-1)**n) * (n * np.pi)**-3
     f2 = np.sin(n * np.pi * np.array([x_]).transpose())
     f3 = np.exp(-(n * np.pi)**2 * i_dt)
 
     return np.sum(f1 * f2 * f3, axis=1)
 
 
-k, L = 0.835, 10
-x_size, t_size = 12, 12
+k, L = 1, 1
+x_size, t_size = 100, 100
 dx, dt = 1 / x_size, 1 / t_size
 x_ = np.linspace(*[0, L], t_size + 1)
 n = np.arange(1, 4001, 1)
 
+c = one_d_crank_nicolson_solver([0, L],
+                                dx,
+                                dt,
+                                t_size,
+                                lambda x: x - x**2,
+                                boundary_conditions=[0, 0])
 
-# c = one_d_crank_nicolson_solver([0, L], dx, dt, t_size, lambda x: 0 * x, boundary_conditions=[100, 50])
-# print(c)
+n_ = 1
 
+f, ax = plt.subplots(1, 3)
+pp = exact_solution(n, x_, k, L, dt * n_)
+ax[0].plot(x_, c[n_], 'r--')
+ax[0].plot(x_, pp, 'b-.')
+ax[1].plot(x_, c[n_] - pp, 'g-.')
 
-# print(c)
-# for i, v in enumerate(c):
-#     plt.plot(x_, v - exact_solution(n, x_, k, L, dt * i))
-#
-# plt.show()
-
-# def onedCN2(x_range, delta_x, delta_t, time_steps, initial_condition, forcing_term=0,
-#             wrap_boundaries=False, n_steps=None, elements=None, tolerance=TOLERANCE,
-#             boundary_conditions=None, has_single_term=True):
-#     fdm_ = OneDimensionalFDM(x_range, delta_x, delta_t, time_steps, forcing_term,
-#                              wrap_boundaries, n_steps, elements,
-#                              tolerance)
-#     r = delta_x / (2 * delta_x**2)
-#     p = tri_diagonal_matrix(fdm_.n_steps, False, [-1, (1 + 2 * r), -1])
-#     q = tri_diagonal_matrix(fdm_.n_steps, False, [1, (1 - 2 * r), 1])
-#     ic_values = np.linspace(*fdm_.pde_properties[0], int(fdm_.n_steps))
-#     ic = initial_condition_matrix(fdm_.n_steps, initial_condition, ic_values)
-#     inv_p = np.linalg.inv(p)
-#     print(inv_p @ (q + ic))
-#     # print(np.linalg.inv(p) @ (q @ ic))
-#
-#
-# onedCN2([0, 1], 0.25, 0.25, 4, lambda x: x)
+plt.show()
