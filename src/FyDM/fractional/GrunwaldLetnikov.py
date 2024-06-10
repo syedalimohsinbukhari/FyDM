7b55436cad550613c5285c818b6eb117931aa936
Syed Ali Mohsin Bukhari <syedali.b@outlook.com>
2024-06-10 15:00:08 +0500

    [oneD-MINOR-20]
    1. testing `GrunwaldLetnikov.py` in `test_.py`

--- src/FyDM/fractional/GrunwaldLetnikov.py
+++ src/FyDM/fractional/GrunwaldLetnikov.py
@@ -2,8 +2,6 @@
 
 import numpy as np
 
-from src.FyDM.__backend.fdm_ import tri_diagonal_matrix
-
 
 def omega(k, alpha):
     """Omega factor."""
@@ -28,9 +26,9 @@ class GrunwaldLetnikov:
         self.ic = initial_conditions
         self.bc = boundary_conditions
 
-    def s_factor(self):
+    def s_factor(self) -> list:
         """S factors for GL fractional order derivative."""
-        return self.T**self.alpha * 0.5 * self.delta_x**-2, self.T**self.alpha * 0.5 * self.delta_y**-2
+        return [self.T**self.alpha * 0.5 * self.delta_x**-2, self.T**self.alpha * 0.5 * self.delta_y**-2]
 
     def omega_factor(self, starting_value=0, summed=True):
         """Calculate the omega factor for GL fractional-order derivative class."""
@@ -43,7 +41,8 @@ class GrunwaldLetnikov:
 
         return np.sum(omega_factor[starting_value:]) if summed else omega_factor[starting_value:]
 
-    def j_matrix(self, n_elements):
+    @staticmethod
+    def j_matrix(n_elements):
         n = n_elements * n_elements
         matrix = np.zeros((n, n))
 
@@ -56,31 +55,45 @@ class GrunwaldLetnikov:
 
         return matrix
 
-    def lhs(self, n_elements):
-        sf = self.s_factor()
-        p1_k1 = tri_diagonal_matrix(n_elements**2, False, elements=[1, 0, 1])
-        p2_k1 = np.eye(n_elements**2)
-        p3_k1 = self.j_matrix(n_elements)
+    def generate_fdm_matrix(self, n, S1, S2, stencil_side='lhs'):
+        N = n * n  # Total number of grid points
+        A = np.zeros((N, N))
 
-        return -sf[0] * p1_k1 + (1 + (2 * sf[0]) + (2 * sf[1])) * p2_k1 - sf[1] * p3_k1
+        diag_val = 1 + 2 * S1 + 2 * S2
+        offsets = [-n, -1, 0, 1, n]  # Corresponding to u_{i,j-1}, u_{i-1,j}, u_{i,j}, u_{i+1,j}, u_{i,j+1}
+        values = [-S2, -S1, diag_val, -S2, -S2] if stencil_side == 'lhs' else [S2, S2, -diag_val, S2, S1]
 
-    def rhs(self, n_elements):
-        sf = self.s_factor()
-        p1_k = tri_diagonal_matrix(n_elements**2, False, elements=[1, 0, 1])
-        p2_k = np.eye(n_elements**2)
-        p3_k = self.j_matrix(n_elements)
+        for i in range(N):
+            for offset, value in zip(offsets, values):
+                j = i + offset
+                if 0 <= j < N:
+                    if (offset == -1 and i % n == 0) or (offset == 1 and (i + 1) % n == 0):
+                        continue
+                    A[i, j] = value
 
-        return sf[0] * p1_k - (-self.alpha + (2 * sf[0]) + (2 * sf[1])) * p2_k + sf[1] * p3_k
+        return A
 
     def solve(self, n_elements):
-        lhs = self.lhs(n_elements)
-        rhs = self.rhs(n_elements)
+        s_factor, omega_factor = self.s_factor(), self.omega_factor
+        lhs = self.generate_fdm_matrix(n_elements, *s_factor, 'lhs')
+        rhs = self.generate_fdm_matrix(n_elements, *s_factor, 'rhs')
 
-        omega_summation = self.omega_factor(2, False)
+        lhs = np.linalg.inv(lhs)
 
-        for k in range(self.T + 1):
-            if k == 2:
-                print(omega_summation[:k])
+        solution = [lhs @ rhs]
+        summation_: list = []
+
+        for k in range(1, self.T):
+            if 1 <= k < 3:
+                s = 2
+                factor = k - s + 1
+                omega_summation = omega_factor(s, False)
+                summation_ = [np.sum(solution[:factor + 1][i] * v, axis=1)
+                              for i, v in enumerate(omega_summation[:factor + 1])]
+
+            solution.append(lhs @ (solution[k - 1] + summation_ if summation_ else solution[k - 1]))
+
+        # return solution
 
 
 gl = GrunwaldLetnikov(0.8,
@@ -95,14 +108,21 @@ gl = GrunwaldLetnikov(0.8,
                           [0, lambda x, y, t: t**2 * np.sin(1) * np.sin(y)],
                           [0, lambda x, y, t: t**2 * np.sin(x) * np.sin(1)]
                       ])
-gl.solve(4)
 
-# print(np.arange(0, 50 + 1 / 4, 1 / 4))
+p = gl.solve(4)
 
-# p1_k = tri_diagonal_matrix(n_elements**2, False, elements=[1, 0, 1])
-# p2_k = np.eye(n_elements**2)
-# p3_k = j_matrix(n_elements)
+# plt.plot(p[0][5])
+# plt.show()
+
+# def exact_solution(x_, y_, i_dt):
+#     return i_dt**2 * np.sin(np.array([x_]).transpose()) * np.sin(y_)
 #
-# rhs = sf[0] * p1_k1 - (-gl.alpha + (2 * sf[0]) + (2 * sf[1])) * p2_k1 + sf[1] * p3_k1
 #
-# print(gl.omega_factor())
+# x = np.linspace(0, 1, 128)
+# y = np.linspace(0, 1, 10)
+# t = np.linspace(0, 1, 10)
+#
+# p = exact_solution(x, 0.1, 1)
+#
+# plt.plot(x, p, 'r-.')
+# plt.show()
